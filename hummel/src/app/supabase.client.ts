14a68658bdaa61dba3ff56de54e7a6fa8331f549
Diego <diego.dinallo10@gmail.com>
2026-04-16 19:44:28 -0300

    modificaciones-deploy

--- hummel/src/app/supabase.client.ts
+++ hummel/src/app/supabase.client.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import { environment } from '../environment/environment';
+import { environment } from './../environments/environment';
 
 export const supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
